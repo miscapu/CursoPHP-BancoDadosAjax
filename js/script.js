9467955e071b30fbed516e738e79c091cccd0081
miscapu <mignan2010@outlook.com>
2018-12-31 13:40:09 -0200

    Script Ajax para solicitar dados da Base de Dados

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-function mostrarUsuarios(str){
-    if (str==""){
-        document.getElementById("txtHint").innerHTML = "";
+function showUser(str) {
+    if (str=="") {
+        document.getElementById("txtHint").innerHTML="";
         return;
     }
     if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
         xmlhttp=new XMLHttpRequest();
-    }else {
+    } else { // code for IE6, IE5
         xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xmlhttp.onreadystatechange=function () {
-        if (this.readyState==4 && this.status==200){
+    xmlhttp.onreadystatechange=function() {
+        if (this.readyState==4 && this.status==200) {
             document.getElementById("txtHint").innerHTML=this.responseText;
         }
     }
-    xmlhttp.open("GET", "muestreUser.php?q="+str, true);
+    xmlhttp.open("GET","template/muestreUser.php?q="+str,true);
     xmlhttp.send();
 }
