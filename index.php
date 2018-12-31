9691976909fa8b9460a8e743b41e9ab5802b281f
miscapu <mignan2010@outlook.com>
2018-12-31 13:35:34 -0200

    Formulário Bootstrap com Select que mostra a solicitação do usuario do aplicativo.

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt">
 <head>
-    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+
     <title>Primeiro Projeto</title>
     <!--Bootstrap-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!--JS-->
+    <script type="application/javascript" src="js/jquery-3.2.1.slim.min.js"></script>
+    <script type="application/javascript" src="js/popper.min.js"></script>
+    <script type="application/javascript" src="js/bootstrap.min.js"></script>
     <!--End Bootstrap-->
+    <script type="application/javascript" src="js/script.js"></script>
+
 </head>
 <body>
 
     <div class="container h-25">
-        <form action="" method="">
-            <select class="form-control">
+        <form>
+            <select  class="form-control" name="users" onchange="showUser(this.value)">
                 <option value="">Selecione um Nome: </option>
                 <option value="1">Miguel Santisteban</option>
                 <option value="2">Nancy Ramos</option>
@@ -23,8 +28,10 @@
             </select>
         </form>
         <br>
-        <div id="txtHint"><b>O nome será listado aquí</b></div>
+
     </div>
+    <div id="txtHint"><b>O nome será listado aquí</b></div>
+
 
 </body>
 </html>
