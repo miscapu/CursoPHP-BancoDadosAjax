c22ab1b51c34d73a9ace86fabe86dd714635f7a9
Miguel Santisteban <mignan2010@outlook.com>
2019-06-17 22:54:42 -0300

    Update muestreUser.php

--- template/muestreUser.php
+++ template/muestreUser.php
@@ -43,11 +43,10 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "bdajax";
-//corrigindo problema de acentos
-$options = array(PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8',);
+
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, $options);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, charset=utf8);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $stmt = $conn->prepare("SELECT * FROM usuarios WHERE id='".$q."'");
     $stmt->execute();
